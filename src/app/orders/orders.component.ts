09b435b80ed043ddcba2ae7bf24411abd2f3efd9
Sachini Chathurangi <chathurangi666@gmail.com>
2019-05-24 18:58:29 +0530

    coffee orders

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit ,Input } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { OrdersService } from "../shared/orders.service";
 
 @Component({
-  selector: 'app-orders',
-  template: '<h2>{{"Hello " + name}}</h2>',
-  styles: []
+  selector: "app-orders",
+  templateUrl: "./orders.component.html",
+  styleUrls: ["./orders.component.scss"]
 })
 export class OrdersComponent implements OnInit {
-  @Input('parentData') public name ;
-  constructor() { }
+  constructor(private ordersService: OrdersService) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  coffees = [
+    "Americano",
+    "Flat White",
+    "Cappuccino",
+    "Latte",
+    "Espresso",
+    "Machiato",
+    "Mocha",
+    "Hot Chocolate",
+    "Tea"
+  ];
+
+  coffeeOrder = [];
+
+  addCoffee = coffee => this.coffeeOrder.push(coffee);
+
+  removeCoffee = coffee => {
+    let index = this.coffeeOrder.indexOf(coffee);
+    if (index > -1) this.coffeeOrder.splice(index, 1);
+  };
+
+  onSubmit() {
+    this.ordersService.form.value.coffeeOrder = this.coffeeOrder;
+    let data = this.ordersService.form.value;
+
+    this.ordersService.createCoffeeOrder(data).then(res => {
+      /*do something here....maybe clear the form or give a success message*/
+    });
   }
-
 }
